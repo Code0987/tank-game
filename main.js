399d22cd5042680f654f5679426802ca4353aca2
Neeraj <durgapalneeraj@gmail.com>
2026-02-05 19:24:33 +0000

    new changes

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: true,
+    menuBarVisible: false, // Hide menu bar for game
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   });
+
+  // Hide the default app menu entirely
+  Menu.setApplicationMenu(null);
 
   mainWindow.loadFile('index.html');
   // mainWindow.webContents.openDevTools();
